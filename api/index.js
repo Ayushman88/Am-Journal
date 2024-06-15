10cf72b52d4bd04dffc3fb6843fd18e0729a5438
Ayushman Mishra <132093864+Ayushman88@users.noreply.github.com>
2024-06-16 00:44:06 +0530

    create a test api

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
 
 dotenv.config();
 
@@ -20,3 +21,5 @@ const port = 3000;
 app.listen(port, () => {
   console.log("Listening on port ", port);
 });
+
+app.use("/api/user", userRoutes);
