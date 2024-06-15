d31133a54082a8f09838a5b80b92c9ce97cb71b2
Ayushman Mishra <132093864+Ayushman88@users.noreply.github.com>
2024-06-16 00:25:01 +0530

    connected to Mongodb

--- api/index.js
+++ api/index.js
@@ -1,6 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("MongoDb Connected");
+  })
+  .catch((err) => {
+    console.error("MongoDb connection error:", err);
+  });
 
 const port = 3000;
 
