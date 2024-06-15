ea10b4e20582d47a832f49a99c1b99f1aebc2735
Ayushman Mishra <132093864+Ayushman88@users.noreply.github.com>
2024-06-15 20:04:04 +0530

    Created pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import Project from "./pages/Project";
+import About from "./pages/About";
+import Error from "./pages/404";
 function App() {
   return (
-    <>
-      <div className="h-screen bg-gray-950"></div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="*" element={<Error />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/project" element={<Project />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
