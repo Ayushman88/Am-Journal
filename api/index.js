c04629c2a32d132c7c358fd5ae2afd6c3530ace4
Ayushman Mishra <132093864+Ayushman88@users.noreply.github.com>
2024-06-18 10:28:14 +0530

    add post and comment route complete create post submit to db

--- api/index.js
+++ api/index.js
@@ -1,41 +1,59 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import cors from "cors"; // Import CORS middleware
+import cors from "cors";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
+import postRoutes from "./routes/post.route.js";
+import commentRoutes from "./routes/comment.route.js";
 import cookieParser from "cookie-parser";
+import path from "path";
+import fs from "fs";
 
 dotenv.config();
 
-const app = express();
-
-// Middleware to parse JSON request bodies
-app.use(express.json());
-app.use(cookieParser());
-
-// Enable CORS middleware
-app.use(
-  cors({
-    origin: "http://localhost:5173", // Allow requests from this origin
-    methods: ["GET", "POST"], // Allow methods
-    allowedHeaders: ["Content-Type"],
-  })
-);
-
-// Connect to MongoDB using Mongoose
 mongoose
-  .connect(process.env.MONGODB_URL, {})
+  .connect(process.env.MONGODB_URL)
   .then(() => {
-    console.log("MongoDB Connected");
+    console.log("MongoDB is connected");
   })
   .catch((err) => {
     console.error("MongoDB connection error:", err);
   });
 
-// Set up routes
+const __dirname = path.resolve();
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cookieParser());
+
+// CORS configuration
+const corsOptions = {
+  origin: "http://localhost:5173", // The URL of your frontend application
+  credentials: true, // Allow credentials (e.g., cookies)
+};
+app.use(cors(corsOptions));
+
+// Routes
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
+app.use("/api/post", postRoutes);
+app.use("/api/comment", commentRoutes);
+
+// Serve static files
+const staticDir = path.join(__dirname, "/client/dist");
+if (fs.existsSync(staticDir)) {
+  app.use(express.static(staticDir));
+
+  // Fallback to serve the index.html for any other routes
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(staticDir, "index.html"));
+  });
+} else {
+  console.error(`Static directory ${staticDir} does not exist`);
+}
 
 // Error handling middleware
 app.use((err, req, res, next) => {
@@ -48,8 +66,8 @@ app.use((err, req, res, next) => {
   });
 });
 
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
