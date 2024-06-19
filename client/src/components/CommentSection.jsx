097c7fee474a68ba87aa9e4516feac9a433a7868
Ayushman Mishra <132093864+Ayushman88@users.noreply.github.com>
2024-06-19 16:36:21 +0530

    add recent post card ui and functionality

--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -131,7 +131,7 @@ const CommentSection = ({ postId }) => {
   return (
     <div className="max-w-2xl mx-auto w-full p-3">
       {currentUser ? (
-        <div className="flex items-center gap-1 my-5 text-gray-500 text-sm">
+        <div className="flex items-center gap-1 my-5 text-gray-300 text-sm">
           <p>Signed in as:</p>
           <img
             className="h-5 w-5 object-cover rounded-full"
@@ -140,7 +140,7 @@ const CommentSection = ({ postId }) => {
           />
           <Link
             to={"/dashboard?tab=profile"}
-            className="text-xs text-cyan-600 hover:underline"
+            className="text-xs text-green-400 hover:underline"
           >
             @{currentUser.username}
           </Link>
@@ -148,7 +148,7 @@ const CommentSection = ({ postId }) => {
       ) : (
         <div className="text-sm text-teal-500 my-5 flex gap-1">
           You must be signed in to comment.
-          <Link className="text-blue-500 hover:underline" to={"/sign-in"}>
+          <Link className="text-rose-500 hover:underline" to={"/sign-in"}>
             Sign In
           </Link>
         </div>
@@ -156,7 +156,7 @@ const CommentSection = ({ postId }) => {
       {currentUser && (
         <form
           onSubmit={handleSubmit}
-          className="border border-teal-500 rounded-md p-3"
+          className="border border-purple-500 rounded-md p-3"
         >
           <Textarea
             placeholder="Add a comment..."
@@ -166,10 +166,10 @@ const CommentSection = ({ postId }) => {
             value={comment}
           />
           <div className="flex justify-between items-center mt-5">
-            <p className="text-gray-500 text-xs">
+            <p className="text-gray-400 text-xs">
               {200 - comment.length} characters remaining
             </p>
-            <Button outline gradientDuoTone="purpleToBlue" type="submit">
+            <Button outline gradientDuoTone="purpleToPink" type="submit">
               Submit
             </Button>
           </div>
@@ -235,7 +235,7 @@ const CommentSection = ({ postId }) => {
   );
 };
 CommentSection.propTypes = {
-  postId: PropTypes.string.isRequired, // Adjust the type as per your application's requirements
+  postId: PropTypes.string.isRequired,
 };
 
 export default CommentSection;
