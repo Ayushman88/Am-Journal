0d0c7606df6efbb06c945f596e5d41dfda116664
Ayushman Mishra <132093864+Ayushman88@users.noreply.github.com>
2024-06-19 17:18:58 +0530

    add project page

--- client/src/pages/Project.jsx
+++ client/src/pages/Project.jsx
@@ -1,5 +1,13 @@
-function Project() {
-  return <div>Project</div>;
-}
+import CallToAction from "../components/CallToAction";
 
-export default Project;
+export default function Projects() {
+  return (
+    <div className="min-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3">
+      <h1 className="text-3xl font-semibold">Pojects</h1>
+      <p className="text-md text-gray-500">
+        Learn Coding in fun and engaging way by following my instagram page!
+      </p>
+      <CallToAction />
+    </div>
+  );
+}
